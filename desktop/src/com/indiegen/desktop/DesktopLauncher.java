cdcea48c60a1f2428f9a7d83bc72a161e7fa3311
Fernando De La Cruz Soto <fdelacruzsoto@gmail.com>
2017-07-13 20:14:51 -0500

    Add desktop support
    Kind of, still not working.

---
+++ desktop/src/com/indiegen/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.indiegen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.indiegen.game.MyGdxGame;
+
+public class DesktopLauncher {
+
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyGdxGame(), config);
+    }
+
+}
